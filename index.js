98322393fa111c867eb698108e2495fadb34d679
Prov <prov.offcl@gmail.com>
2021-09-29 14:05:35 +0300

    3.17*: Phonebook database, step5

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get('/api/persons', (req, res) => {
         .then(persons => res.json(persons))
 })
 
-app.get('/api/persons/:id', (req, res) => {
+app.get('/api/persons/:id', (req, res, next) => {
     Person.findById(req.params.id)
         .then(person => {
             if(person){
@@ -30,7 +30,7 @@ app.get('/api/persons/:id', (req, res) => {
         .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
+app.delete('/api/persons/:id', (req, res, next) => {
     Person.findByIdAndRemove(req.params.id)
         .then(result => {
             res.status(204).end()
@@ -53,6 +53,19 @@ app.post('/api/persons', (req, res) => {
     person.save().then(savedPerson => {
         res.json(savedPerson)
     })
+})
+
+app.put('/api/persons/:id', (req, res, next) => {
+    const newPerson = {
+        name: req.body.name,
+        number: req.body.number
+    }
+    Person
+        .findByIdAndUpdate(req.params.id, newPerson, { new: true })
+        .then(updatedPerson => {
+            res.json(updatedPerson)
+        })
+        .catch(error => next(error))
 })
 
 const unknownEndpoint = (req, res) => {
