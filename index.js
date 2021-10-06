2bed39c3b80a0c1b3773f9e86811ef80b3a38043
Prov <prov.offcl@gmail.com>
2021-10-06 22:01:51 +0300

    3.22: Lint configuration

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const app = express()
 const Person = require('./models/person')
 
+// eslint-disable-next-line no-unused-vars
 morgan.token('data', (req, res) => req.method === 'POST' ? JSON.stringify(req.body) : null)
 
 app.use(express.static('build'))
@@ -13,9 +14,9 @@ app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
 app.get('/info', (req, res, next) => {
-    Person.find({})
-        .then(persons => {
-            res.send(`
+	Person.find({})
+		.then(persons => {
+			res.send(`
                 <div>
                     Phonebook has info for ${persons.length} people
                 </div>
@@ -24,83 +25,81 @@ app.get('/info', (req, res, next) => {
                     ${new Date()}
                 </div>
             `)
-        })
-        .catch(error => next(error))
+		})
+		.catch(error => next(error))
 })
 
 app.get('/api/persons', (req, res) => {
-    Person
-        .find({})
-        .then(persons => res.json(persons))
+	Person
+		.find({})
+		.then(persons => res.json(persons))
 })
 
 app.get('/api/persons/:id', (req, res, next) => {
-    Person.findById(req.params.id)
-        .then(person => {
-            if(person){
-                res.json(person)
-            } else{
-                res.status(404).end()
-            }
-        })
-        .catch(error => next(error))
+	Person.findById(req.params.id)
+		.then(person => {
+			if(person){
+				res.json(person)
+			} else{
+				res.status(404).end()
+			}
+		})
+		.catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (req, res, next) => {
-    Person.findByIdAndRemove(req.params.id)
-        .then(result => {
-            res.status(204).end()
-        })
-        .catch(error => next(error))
+	Person.findByIdAndRemove(req.params.id)
+		.then(() => {
+			res.status(204).end()
+		})
+		.catch(error => next(error))
 })
 
 app.post('/api/persons', (req, res, next) => {
-    const body = req.body;
+	const body = req.body
 
-    const person = new Person({
-        name: body.name,
-        number: body.number,
-    })
+	const person = new Person({
+		name: body.name,
+		number: body.number,
+	})
 
-    person.save()
-        .then(savedPerson => {
-            res.json(savedPerson)
-        })
-        .catch(error => next(error))
+	person.save()
+		.then(savedPerson => {
+			res.json(savedPerson)
+		})
+		.catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (req, res, next) => {
-    console.log('put')
-    const newPerson = {
-        name: req.body.name,
-        number: req.body.number
-    }
-    Person
-        .findByIdAndUpdate(req.params.id, newPerson, { new: true })
-        .then(updatedPerson => {
-            res.json(updatedPerson)
-        })
-        .catch(error => {
-            console.log('put error')
-            next(error)})
+	console.log('put')
+	const newPerson = {
+		name: req.body.name,
+		number: req.body.number
+	}
+	Person
+		.findByIdAndUpdate(req.params.id, newPerson, { new: true })
+		.then(updatedPerson => {
+			res.json(updatedPerson)
+		})
+		.catch(error => next(error))
 })
 
 const unknownEndpoint = (req, res) => {
-    res.status(404).send({ error: 'unknown endpoint' })
+	res.status(404).send({ error: 'unknown endpoint' })
 }
 
 const errorHandler = (err, req, res, next) => {
-    console.log(err.message)
+	console.log(err.message)
 
-    console.log(err.name)
+	console.log(err.name)
 
-    if(err.name === 'CastError') {
-        return res.status(400).send({ error: 'malformatted id' })
-    } else if(err.name === 'ValidationError') {
-        return res.status(400).json({ error: err.message })
-    }
+	if(err.name === 'CastError') {
+		return res.status(400).send({ error: 'malformatted id' })
+	} else if(err.name === 'ValidationError') {
+		return res.status(400).json({ error: err.message })
+	}
 
-    next(error)
+	next(err)
 }
 
 app.use(unknownEndpoint)
@@ -108,5 +107,5 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+	console.log(`Server running on port ${PORT}`)
 })
